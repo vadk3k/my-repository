f1cd49f18c7193e0e8a0f12051b9ee8f8abb11ab
Angineh Vartanian <angineh.v@gmail.com>
2025-02-03 07:07:57 -0800

    add flask 3,4

---
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -0,0 +1,19 @@
+# Import Flask modules
+
+
+# Create an object named app 
+
+
+
+# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# and assign to the route of ('/')
+
+
+
+# Create a function named header which prints numbers elements of list one by one in `index.html` 
+# and assign to the route of ('/')
+
+
+
+# run this app in debug mode on your local.
+
