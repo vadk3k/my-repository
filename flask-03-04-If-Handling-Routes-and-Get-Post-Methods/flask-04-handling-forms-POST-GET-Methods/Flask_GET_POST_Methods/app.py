4b040e8f57f024a500ce0030fbef4f4f3ce3e20f
Angineh Vartanian <angineh.v@gmail.com>
2025-02-09 17:08:00 -0800

    final push

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -1,25 +1,35 @@
 # Import Flask modules
-
+from flask import Flask, render_template, request
 
 # Create an object named app
+app = Flask(__name__)
 
-
-
-# create a function named "lcm" which calculates a least common multiple values of two numbers. 
-
-
-
+# create a function named "multiply" which multiplies two numbers.
+def multiply(a,b):
+    return a*b
+ 
 # Create a function named `index` which uses template file named `index.html` 
-# send two numbers as template variable to the app.py and assign route of no path ('/') 
+# and assign route of no path ('/') 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-
-
-
-
-# calculate sum of them using "lcm" function, then sent the result to the 
+# calculate product of them using "multiply" function, then sent the result to the 
 # "result.hmtl" file and assign route of path ('/calc'). 
-# When the user comes directly "/calc" path, "Since this is a GET request, LCM has not been calculated" string returns to them with "result.html" file
+# When the user comes directly "/calc" path, "Since this is a GET request, multiplication is not calculated" string returns to them with "result.html" file
+@app.route("/calc", methods=["GET", "POST"])
+def calc():
 
-
+ if request.method == "POST":
+    #this is a POST request
+    #multiply num1 x num2
+    num1 = request.form.get("number1")
+    num2 = request.form.get("number2")
+    c = multiply(int(num1), int(num2))
+    return render_template("result.html", a=num1, b=num2, result=str(c), developer_name="Angineh")
+ else:
+    #this is a GET request
+    return render_template("result.html", developer_name="Angineh")
 
 # Add a statement to run the Flask application which can be debugged.
+app.run(debug=True)
--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -1,21 +1,45 @@
 # Import Flask modules
-
+from flask import Flask, render_template, redirect, url_for, request
 
 # Create an object named app
-
+app = Flask(__name__)
 
 # Create welcome page with main.html file and assign it to the root path
-
+@app.route("/")
+def welcom():
+    return render_template("main.html", name="Angineh")
 
 # Write a function named `greet` which uses template file named `greet.html` given under 
 # `templates` folder. it takes parameters from query string on URL, assign that parameter 
 # to the 'user' variable and sent that user name into the html file. If it doesn't have any parameter, warning massage is raised
-
-
+@app.route("/greet")
+def greet():
+    
+    if "user" in request.args:
+        the_user = request.args.get("user")
+        return render_template("greet.html", user=the_user)
+    else:
+        return "<h1>Missing user in query string</h1>"
 
 # Write a function named `login` which uses `GET` and `POST` methods, 
 # and template files named `login.html` and `secure.html` given under `templates` folder 
 # and assign to the static route of ('login')
+@app.route("/login", methods=["GET", "POST"])
+def login():
 
+    if request.method == "POST":
+        #then POST method
+        user_name = request.form.get("username")
+        pw = request.form.get("password")
+        if user_name == "clarusway" and pw =="clarusway":
+            #this is the correct username and password
+            return render_template("secure.html", user=user_name)
+        else:
+            #this is incorrect
+            return render_template("login.html", control=1, user=user_name)     
+    else:
+        #the GET method
+        return render_template("login.html")
 
 # Add a statement to run the Flask application
+app.run(debug=True)
